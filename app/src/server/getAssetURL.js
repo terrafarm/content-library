b21389535b78f6c25111d758b126f571d0d8b8c9
Ryan Blakeley <rbblakeley@gmail.com>
2017-01-28 12:45:16 -0700

    Disabled CDN asset urls

--- app/src/server/getAssetURL.js
+++ app/src/server/getAssetURL.js
@@ -1,16 +1,20 @@
 import hashString from './hashString';
 
+// DISABLED but worth considering in the future
+/*
 const CDN_HOSTS = [
   'd3ogqji57fkqg9.cloudfront.net',
   'd19zmavcjzsuj4.cloudfront.net',
   'djfaa0bz60cz6.cloudfront.net',
   'd2tdr4rkgjw2gh.cloudfront.net',
 ];
-
+*/
 /**
  * In production returns a CDN URL; otherwise returns the URL as is.
  */
 export default function getAssetURL(asset: string): string {
+  return asset;
+/*
   if (__DEV__) {
     return asset;
   } else if (asset.startsWith('/')) {
@@ -19,4 +23,5 @@ export default function getAssetURL(asset: string): string {
   } else {
     return asset;
   }
+*/
 }
